3d38a1053dc555bf71cc518de97ca111bafee590
jianyuan <18672793611@163.com>
2019-05-10 18:17:39 +0800

    第二次提交

--- src/main/java/com/rabbit/demo/controller/AmqpController.java
+++ src/main/java/com/rabbit/demo/controller/AmqpController.java
@@ -1,10 +1,12 @@
 package com.rabbit.demo.controller;
 
 import com.rabbit.demo.service.AmqpService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("mq")
@@ -22,6 +24,18 @@ public class AmqpController {
     public void  topic(String rounteKey){
         amqpService.topic(rounteKey );
     }
-
+    @GetMapping("test")
+    public void map(HttpServletRequest request){
+        System.out.println(request.getParameter("latitude"));
+        System.out.println(request.getParameter("appstate"));
+        System.out.println(request.getParameter("phone"));
+        System.out.println(request.getParameter("userId"));
+        System.out.println(request.getParameter("longitude"));
+    }
+    @RequestMapping("test2")
+    public void map(String phone,String uid){
+        System.out.println(phone);
+        System.out.println(uid);
+    }
 
 }
