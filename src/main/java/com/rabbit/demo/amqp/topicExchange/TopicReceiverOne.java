85d1e4eedf9d347fec8d8816e999756bd65928ab
yuanjian <18672793611@163.com>
2020-10-09 16:34:06 +0800

    重试机制配置

--- src/main/java/com/rabbit/demo/amqp/topicExchange/TopicReceiverOne.java
+++ src/main/java/com/rabbit/demo/amqp/topicExchange/TopicReceiverOne.java
@@ -13,7 +13,7 @@ public class TopicReceiverOne {
 
     private static int num=3;
 
-    @RabbitListener(queues = RabbitTopicConfig.IG)
+//    @RabbitListener(queues = RabbitTopicConfig.IG)
     public void process1(RabbitParams params, Message message, Channel channel) {
 //        System.out.println(params);
         System.out.println("接收者1  " + params);
@@ -21,21 +21,27 @@ public class TopicReceiverOne {
     }
 
     @RabbitListener(queues = RabbitTopicConfig.IG)
-    public void process2(RabbitParams params, Message message, Channel channel) {
+    public void process2(RabbitParams params, Message message, Channel channel) throws IOException {
         try {
-            System.out.println(num);
             num-=1;
-            int a=10/num;
+            System.out.println(num);
+            int a=10/0;
             System.out.println("接收者2  " + params);
-
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),true);
         } catch (Exception e) {
-            try {
+            System.out.println("error");
+//            try {
                 //  当  num=0; 发生异常  重新入队列  立马就被监听消费。
-                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            e.printStackTrace();
+                /**
+                 * 当在yml 配置了 重试机制后 这里重新入队就不要配置了 新入队后 又会重试  导致无限先死循环
+                 */
+//                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+//            } catch (IOException e1) {
+//                System.out.println("1111111111");
+//                e1.printStackTrace();
+//            }
+            System.out.println("消息丢弃了.................."+params);
+            throw e;
         }
 //        System.out.println("接收者2  " + message);
     }
